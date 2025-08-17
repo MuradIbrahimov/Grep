db30a78860f2a35b8869f5f271587d1d3604b7ae
MuradIbrahimov <104825680+MuradIbrahimov@users.noreply.github.com>
2025-08-17 15:32:09 +0400

    App is kinda works

--- ui/src/routes/root.routes.tsx
+++ ui/src/routes/root.routes.tsx
@@ -1,12 +1,8 @@
-import React from "react";
-
 import { Routes, Route } from "react-router-dom";
 
 import {
   Home,
 } from "../pages";
-
-import "../styles/_globals.css";
 
 const Routing = () => {
   return (
